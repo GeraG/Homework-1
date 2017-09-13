011e004e12f62e4d45a25ee405df77bc6ffe7757
Gera Groshev <groshevg@berkeley.edu>
2017-09-13 16:53:11 -0700

    completed hw1

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -2,14 +2,24 @@ pragma solidity ^0.4.13;
 
 
 contract greeter {
-	/* Add one variable to hold our greeting */
-	string greeting;
+    /* Add one variable to hold our greeting */
+    string greeting;
+    uint greetCounter;
 
-	function greeter(string _greeting) public {
-		/* Write one line of code for the contract to set our greeting */
-	}
+    event Send(string _greeting);
 
-	function greet() constant returns (string)  {
-		/* Write one line of code to allow the contract to return our greeting */
-	}
+    function greeter(string _greeting) public {
+        /* Write one line of code for the contract to set our greeting */
+        greeting = _greeting;
+        greetCounter = 0;
+    }
+
+    function greet() constant returns (string)  {
+        /* Write one line of code to allow the contract to return our greeting */
+        greetCounter += 1;
+        Send(greeting);
+        return greeting;
+    }
+
+    function () {}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,9 +2,84 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
-	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
-	}
+    
+    mapping (uint => uint) public fibResults;
+    uint lastPositionCalculated;
+    
+    function Fibonacci() public {
+        fibResults[0] = 0;
+        fibResults[1] = 1;
+        lastPositionCalculated = 1;
+    }
+    
+    event publishFib(uint result);
+    
+    function calculate(uint position) returns (uint result) {
+        /* Add one variable to hold our greeting */
+        // result = calcMathematically(position); // optimal O(1), thus potentially uses much less gas for large positions
+        result = calcSequentially(position);
+        publishFib(result);
+    }
 
-	/* Add a fallback function to prevent contract payability and non-existent function calls */
+    function calcSequentially(uint position) private returns (uint result) {
+        if (position == 0)
+            result = 0;
+        else if (position == 1)
+            result = 1;
+        else if (position <= lastPositionCalculated)
+            result = fibResults[position];
+        else {
+            while (lastPositionCalculated < position) {
+                lastPositionCalculated += 1;
+                fibResults[lastPositionCalculated] = fibResults[lastPositionCalculated - 1] + fibResults[lastPositionCalculated - 2];
+            }
+            result = fibResults[position];
+        }
+    }
+    
+    function calcMathematically(uint position) private returns (uint result) {
+        if (position == 0) {
+            result = 0;
+            return;
+        } else if (fibResults[position] > 0) {
+            result = fibResults[position];
+            return;
+        }
+        
+        fixed goldenRatio = (1 + sqrt(5)) / 2;
+        result = uint((pow(goldenRatio, int(position)) - (1 / pow(-goldenRatio, int(position)))) / sqrt(5));
+    }
+    
+    function sqrt(int n) private returns (fixed)
+    {
+      // Babylonian square root
+      // Use n as initial approximation
+      fixed x = fixed(n);
+      fixed y = 1;
+      fixed e = 0.0000001; /* e decides the accuracy level*/
+      while(x - y > e)
+      {
+        x = (x + y) / 2;
+        y = fixed(n) / x;
+      }
+      return x;
+    }
+    
+    function pow(fixed base, int exp) private returns (fixed)
+    {
+        fixed result = 1;
+        while (exp > 0) {
+            if (exp & 1 == 1) {
+                result *= base;
+            }
+            base *= base;
+            exp /= 2;
+        }
+
+        return result;
+    }
+
+    /* Add a fallback function to prevent contract payability and non-existent function calls */
+    
+    function () {}
 }
