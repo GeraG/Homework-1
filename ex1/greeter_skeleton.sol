d4c1fe9cccfda6e1ef93cf6f285d7a8b86cf802a
Gera Groshev <groshevg@berkeley.edu>
2017-09-20 23:12:01 -0700

    naming convention update

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.13;
 
 
-contract greeter {
+contract Greeter {
     /* Add one variable to hold our greeting */
     string greeting;
     uint greetCounter;
 
     event Send(string _greeting);
 
-    function greeter(string _greeting) public {
+    function Greeter(string _greeting) public {
         /* Write one line of code for the contract to set our greeting */
         greeting = _greeting;
         greetCounter = 0;
